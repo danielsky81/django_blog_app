7604c4d196eff029cbd3f6befd7003b60d92bdc1
danielsky <karma0rg4u@gmail.com>
2019-06-10 11:59:35 +0100

    create view, url and 1st template

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
